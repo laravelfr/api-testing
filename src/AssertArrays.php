de6af0399f3f15ec823e6d98e240e7dd7de47537
Mathieu TUDISCO <mathieu.tu@gmail.com>
2016-11-23 15:52:26 +0100

    ✨ Add assertions for arrays

---
+++ src/AssertArrays.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace LaravelFr\ApiTests;
+
+trait AssertArrays
+{
+    /**
+     * Assert that the given array has exactly the given structure.
+     *
+     * @param  array $structure
+     * @param  array $array
+     *
+     * @return $this
+     */
+    public function assertArrayStructureEquals(array $structure, array $array)
+    {
+        $structureFirstLevel = array_map(function ($value, $key) {
+            return is_array($value) ? $key : $value;
+        }, $structure, array_keys($structure));
+
+        $responseFirstLevel = array_keys($array);
+
+        if ($structureFirstLevel !== ['*']) {
+            $this->assertEquals($structureFirstLevel, $responseFirstLevel, '', 0.0, 10, true);
+        }
+
+        $structureOtherLevels = array_filter($structure, function ($value) {
+            return is_array($value);
+        });
+
+        foreach ($structureOtherLevels as $key => $childStructure) {
+            if ($key === '*') {
+                $this->assertInternalType('array', $array);
+
+                foreach ($array as $responseDataItem) {
+                    $this->assertArrayStructureEquals($childStructure, $responseDataItem);
+                }
+            } else {
+                $this->assertArrayStructureEquals($childStructure, $array[$key]);
+            }
+        }
+
+        return $this;
+    }
+}
