22c2551ddea15408dfe843288745808e19c57422
Mathieu TUDISCO <mathieu.tu@gmail.com>
2016-11-23 15:56:45 +0100

    ✅ Add tests for arrays assertions

---
+++ tests/AssertArraysTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LaravelFr\Tests;
+
+use JsonSerializableMixedResourcesStub;
+use LaravelFr\ApiTests\AssertArrays;
+use PHPUnit_Framework_TestCase;
+
+class AssertArraysTest extends PHPUnit_Framework_TestCase
+{
+    use AssertArrays;
+
+    public function testAssertArrayStructureEquals()
+    {
+        $array = (new JsonSerializableMixedResourcesStub)->jsonSerialize();
+
+        $this->assertArrayStructureEquals([
+            'foo',
+            'baz'    => ['*' => ['bar' => ['*'], 'foo']],
+            'bars'   => ['*' => ['bar', 'foo']],
+            'foobar' => ['foobar_foo', 'foobar_bar'],
+        ], $array);
+    }
+}
