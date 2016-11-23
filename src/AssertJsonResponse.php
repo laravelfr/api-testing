2aa263fab6034de4011cfbf3f0f00939406c923a
Mathieu TUDISCO <mathieu.tu@gmail.com>
2016-11-23 16:03:44 +0100

    ✨ Add assertions for JsonResponses

---
+++ src/AssertJsonResponse.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaravelFr\ApiTests;
+
+use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
+
+trait AssertJsonResponse
+{
+    use AssertArrays, MakesHttpRequests;
+
+    /**
+     * Assert that the JSON response has exactly the given structure.
+     *
+     * @param  array $structure
+     * @param  array|null $responseData
+     *
+     * @return $this
+     */
+    public function seeJsonStructureEquals(array $structure, $responseData = null)
+    {
+        if (!$responseData) {
+            $responseData = $this->decodeResponseJson();
+        }
+
+        return $this->assertArrayStructureEquals($structure, $responseData);
+    }
+
+    /**
+     * Return the json response or a part of it as an array.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function jsonResponse($key = null)
+    {
+        $response = $this->decodeResponseJson();
+
+        return $key ? array_get($response, $key) : $response;
+    }
+}
