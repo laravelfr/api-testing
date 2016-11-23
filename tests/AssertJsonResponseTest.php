2f13bdae6d18b7cf77f980d41e45db0e49bf8a13
Mathieu TUDISCO <mathieu.tu@gmail.com>
2016-11-23 16:04:31 +0100

    ✅ Add tests for JsonResponses assertions

---
+++ tests/AssertJsonResponseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LaravelFr\Tests;
+
+use JsonSerializableMixedResourcesStub;
+use LaravelFr\ApiTests\AssertJsonResponse;
+use PHPUnit_Framework_TestCase;
+
+class AssertArraysTest extends PHPUnit_Framework_TestCase
+{
+    use AssertJsonResponse;
+
+    public function testAssertArrayStructureEquals()
+    {
+        $this->response = new \Illuminate\Http\Response(new JsonSerializableMixedResourcesStub);
+
+        $this->seeJsonStructureEquals([
+            'foo',
+            'baz'    => ['*' => ['bar' => ['*'], 'foo']],
+            'bars'   => ['*' => ['bar', 'foo']],
+            'foobar' => ['foobar_foo', 'foobar_bar'],
+        ]);
+    }
+
+    public function testJsonResponse()
+    {
+        $this->response = new \Illuminate\Http\Response(new JsonSerializableMixedResourcesStub);
+
+        $this->assertEquals(
+            (new JsonSerializableMixedResourcesStub)->jsonSerialize(),
+            $this->jsonResponse()
+        );
+        $this->assertEquals(
+            ['foobar_foo' => 'foo', 'foobar_bar' => 'bar'],
+            $this->jsonResponse('foobar')
+        );
+        $this->assertEquals('bar', $this->jsonResponse('foobar.foobar_bar'));
+
+    }
+}
